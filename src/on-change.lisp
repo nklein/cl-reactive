69133bdb217e7d49cd4746265f84f7ed5a2055a9
Patrick Stein <pat@nklein.com>
2013-10-09 23:02:46 -0500

    Added key option to on-change.

--- src/on-change.lisp
+++ src/on-change.lisp
@@ -4,20 +4,22 @@
 
 (defun signal-on-change (sig &key
                                (test #'equal)
+                               (key #'identity)
                                documentation)
-  "Create a signal function that returns the value of SIG but that only triggers dependents to update when its value has actually changed.  The given TEST function is used to tell if two values are equal.  The default TEST is #'EQUAL."
+  "Create a signal function that returns the value of SIG but that only triggers dependents to update when its value has actually changed.  The given TEST function is used to tell if two values are equal.  The KEY function is called on the old value and new value and the results are passed to the TEST function to tell if the results are equal.  The default TEST is #'EQUAL and the default KEY is #'IDENTITY."
   (let* ((documentation (or documentation
                             (format nil "~A" `(ON-CHANGE ,sig))))
          (int-doc (concatenate 'string "Internal: " documentation))
-         (value (signal-value sig))
+         (value (funcall key (signal-value sig)))
          (sig-internal (signal-variable value
                                          :type (signal-type sig)
                                          :documentation int-doc)))
     (signal-flet ((sig-update ((v sig))
-                              (unless (funcall test value v)
-                                (with-signal-values ((int sig-internal))
-                                  (setf value v
-                                        int v)))
+                              (let ((k (funcall key v)))
+                                (unless (funcall test value k)
+                                  (with-signal-values ((int sig-internal))
+                                    (setf value k
+                                          int v))))
                               v))
       ;; Here, it would be nice to just return sig-internal and
       ;; be done with it.  But, if we did that, then no one would
